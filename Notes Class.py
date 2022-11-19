a073f2e48aa43e2cf0c40a896db6cb67304e0669
John Liu <johnliuchs2022@gmail.com>
2022-11-19 16:01:50 -0500

    Created Notes Parent Class

---
+++ Notes Class.py
@@ -0,0 +1,24 @@
+class Notes(object):
+    def __init__ (self, xPos, score = 20, noteLength = 1):
+        self.x = xPos
+        self.noteLength = noteLength
+        self.y = 0
+        self.score = score
+
+
+#Function draws the note's current position
+    def drawNote(self, canvas):
+        cx, cy = self.x, self.y
+
+
+    def updateNotePos(self):
+        self.y += 1
+
+#Function takes in player input and matches it with the note's x-position,
+    def scoreNote(self, playerInput):
+        #Check player input algorithm later TODO
+        input1, input2 = playerInput[0], playerInput[1]
+        if self.x == input1 or self.x == input2:
+            return self.score
+        #return 0 if the player input is not close enough
+        return 0  
